e2e41808de1255d969b60199552b0e68712974c3
HienNV9x <hiennv.9x@gmail.com>
2024-07-07 22:41:35 +0700

    Update Project

--- src/main/java/com/viettravelbk/controller/UserController.java
+++ src/main/java/com/viettravelbk/controller/UserController.java
@@ -1,8 +1,8 @@
 package com.viettravelbk.controller;
 
 import java.security.Principal;
-import java.util.HashMap; //
-import java.util.Map; //
+import java.util.HashMap; 
+import java.util.Map; 
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -13,12 +13,12 @@ import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.validation.annotation.Validated; //
+import org.springframework.validation.annotation.Validated; 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody; //
-import org.springframework.web.bind.annotation.RequestParam; //
+import org.springframework.web.bind.annotation.RequestBody; 
+import org.springframework.web.bind.annotation.RequestParam; 
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.viettravelbk.service.CustomUserDetails;
